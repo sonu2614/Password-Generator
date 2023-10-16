2241f67441dfcda395afd0141cc30571772e91b5
Sonu <chaudharysonu2614@gmail.com>
2023-10-16 16:49:07 +0530

    password

--- src/App.js
+++ src/App.js
@@ -1,23 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useCallback, useEffect, useRef } from "react";
+
+
+//-----------------------------------------------------useState--------------------------------------------
 
 function App() {
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  //-----------------------------------------------------useRef--------------------------------------------
+
+  const passwordRef=useRef(null)
+  
+  const copyPasswordToClipboard=useCallback(()=>{
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0,3)
+    window.navigator.clipboard.writeText(password)
+  },[password])
+  //-----------------------------------------------------useCallback--------------------------------------------
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numberAllowed) str += "0123456789";
+    if (charAllowed) str += "!@#$%^&*-_+=[]{}~`";
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+
+      pass += str.charAt(char);
+    }
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed, setPassword]);
+
+  //-----------------------------------------------------useEffect--------------------------------------------
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numberAllowed, charAllowed, passwordGenerator]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      className="w-full max-w-md mx-auto shadow-md
+    rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500"
+    >
+      <h1
+        className="text-4xl text-center 
+      mt-10 text-white"
+      >
+        Password Generator
+      </h1>
+      <div
+        className="flex shadow rounded-lg
+      overflow-hidden mb-4"
+      >
+        <input
+          type="text"
+          value={password}
+          className="outline-none w-full py-1 px-3"
+          placeholder="Password"
+          readOnly
+          ref={passwordRef}
+        />
+        <button
+        onClick={copyPasswordToClipboard}
+          className="outline-none bg-blue-500
+        text-white px-3 shrink-0"
         >
-          Learn React
-        </a>
-      </header>
+          Copy
+        </button>
+      </div>
+      <div className="flex text-sm gap-x-2">
+        <div className="flex items-center gap-x-1">
+          <input
+            type="range"
+            min={8}
+            max={50}
+            value={length}
+            className="cursor-pointer"
+            onChange={(e) => {
+              setLength(e.target.value);
+            }}
+          />
+          <label>Length : {length}</label>
+        </div>
+        <div className="flex items-center gap-1">
+          <input
+            type="checkbox"
+            defaultChecked={numberAllowed}
+            id='"numberInput'
+            onChange={() => {
+              setNumberAllowed((prev) => !prev);
+            }}
+          />
+          <label htmlFor="numberInput">Numbers</label>
+        </div>
+        <div className="flex items-center gap-1">
+          <input
+            type="checkbox"
+            defaultChecked={charAllowed}
+            id="characterInput"
+            onChange={() => {
+              setCharAllowed((prev) => !prev);
+            }}
+          />
+          <label htmlFor="characterInput">Characters</label>
+        </div>
+      </div>
     </div>
   );
 }
